f7dd3d2c199ccbcbf5a6f1515bf8c3c23cce932a
Fido8760 <fidel050987@gmail.com>
2024-12-05 13:12:37 -0600

    first commit

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const conectarDB = async () => {
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI);
+
+        const url = `${db.connection.host}:${db.connection.port}`;
+
+        console.log(`MongoDB conectado en: ${url}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Terminar proceso si hay error
+    }
+};
+
+export default conectarDB;
